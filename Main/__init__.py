863fe0ab36732df60b64fff9d284853600f2af1e
Bradley-B <brad.boxer1@gmail.com>
2019-02-18 01:07:15 -0500

    Caching
    Every time a request is made to the RIT dining website,
    the processed result is stored. If another user asks for
    the same page it is read from the cache instead of submitting
    another request.

--- Main/__init__.py
+++ Main/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request
 from .parser import GraciesHtmlParser
 from .button import ButtonObject
+from typing import List
 import urllib.parse
 import urllib.request
 import datetime
@@ -19,7 +20,39 @@ def post(url: str, values) -> str:
 	req = urllib.request.Request(url, data)
 	return urllib.request.urlopen(req).read().decode('utf-8')
 	
+
+def setup_buttons() -> List[ButtonObject]:
+	buttons = []
+	today = datetime.datetime.now()
+	for i in range(7):
+		target_day = today + datetime.timedelta(days = i)
+		buttons.append(ButtonObject(target_day.strftime("%Y-%m-%d")))
+	return buttons
+
+
+def save_cache(obj):
+	with open('/remote/testapi/Main/obj/' + 'gracies_menu_cache' + '.pkl', 'wb') as f:
+		pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
 	
+def load_cache():
+	with open('/remote/testapi/Main/obj/' + 'gracies_menu_cache' + '.pkl', 'rb') as f:
+		return pickle.load(f)
+	
+
+def get_menu_content(date):
+	if date in cache:
+		return cache[date]
+	else:
+		values = {'menu_date': date}
+		html = post('https://www.rit.edu/fa/diningservices/gracies', values)
+		parser = GraciesHtmlParser()
+		parser.feed(html)
+		cache[date] = parser.get_output()
+		save_cache(cache)
+		return parser.get_output()
+
+
 @app.route("/")
 def root():
 	return redirect("http://bradsraspi.student.rit.edu/gracies", code=302)
@@ -27,24 +60,13 @@ def root():
 
 @app.route("/gracies", methods=['POST', 'GET'])
 def gracies():
-	
 	if request.method =='POST':
-		values = {"menu_date": request.form['button']}
-		html = post('https://www.rit.edu/fa/diningservices/gracies', values)
 		date_displayed = request.form['button']
 	else:
-		html = get('https://www.rit.edu/fa/diningservices/gracies')
 		date_displayed = datetime.datetime.now().strftime("%Y-%m-%d")
 	
-	parser = GraciesHtmlParser()
-	parser.feed(html)
+	return render_template('gracies.html', content = get_menu_content(date_displayed), buttons = setup_buttons(), date = date_displayed)
+
 	
-	buttons = []
-	today = datetime.datetime.now()
-	for i in range(7):
-		target_day = today + datetime.timedelta(days = i)
-		buttons.append(ButtonObject(target_day.strftime("%Y-%m-%d")))
-	
-	return render_template('gracies.html', content = parser.get_output(), buttons = buttons, date = date_displayed)
-	
-	
+cache = load_cache()
+
